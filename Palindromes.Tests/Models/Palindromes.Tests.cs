721162ebcf6b7770b74b94b0f20e81b66dc3b928
Skylan Lew <skylanslew@gmail.com>
2022-09-20 11:02:19 -0700

    init
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>
    Co-authored-by: William Mentzer <william.mentzer2k@gmail.com>

---
+++ Palindromes.Tests/Models/Palindromes.Tests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindromes;
+
+namespace Palindromes.Tests
+{
+  [TestClass]
+  public class PalindromesTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      //testing code will go here
+     
+    }
+  }
+}
---
+++ Palindromes/Models/Palindromes.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Palindromes
+{
+  public class Palindromes
+  {
+    public static void Main()
+    {
+    
+    }
+
+    public Palindromes()
+    {
+      
+    }
+  }
+
+
+}
