461d8d30ae119a42ab8c6369ba1637edb47d5284
Skylan Lew <skylanslew@gmail.com>
2022-09-20 16:10:40 -0700

    pass last 2 tests, learn about ref and more about types
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>
    Co-authored-by: William Mentzer <william.mentzer2k@gmail.com>

--- Palindromes.Tests/Models/Palindromes.Tests.cs
+++ Palindromes.Tests/Models/Palindromes.Tests.cs
@@ -1,7 +1,12 @@
+using System.Runtime;
 using System.Runtime.InteropServices;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Palindromes;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.Linq;
 
 namespace Palindromes.Tests
 {
@@ -17,13 +22,28 @@ namespace Palindromes.Tests
       CollectionAssert.AreEqual(array, myPalindrome.StringToArray());
 
     }
-
+    [TestMethod]  
     public void FlipTheArray_FlipsTheArray_yarra()
     {
       Palindromes myPalindrome = new Palindromes("array");
-      char[] yarra = new char[] {'y', 'a', 'r', 'r', 'a'};
-      CollectionAssert.AreEqual(yarra, myPalindrome.FlipTheArray());
+      List<char> yarra = new List<char>(){'y','a', 'r', 'r', 'a'};
+      char[] myPalindromeArray = myPalindrome.StringToArray();
+      CollectionAssert.AreEqual(yarra, Palindromes.FlipTheArray(ref myPalindromeArray));
     }
-
+    [TestMethod]
+    public void IsPalindrome_ChecksIfPalindrome_TRUE()
+    {
+      Palindromes myPalindrome = new Palindromes("raceCar");
+      char[] myPalindromeArray = myPalindrome.StringToArray();
+      Assert.AreEqual(true, myPalindrome.IsPalindrome(ref myPalindromeArray));
+    }
+    
+    [TestMethod]
+    public void IsPalindrome_ChecksIfPalindrom_FALSE()
+    {
+      Palindromes myPalindrome = new Palindromes("supercalifragilisticexpialidocious");
+      char[] myPalindromeArray = myPalindrome.StringToArray();
+      Assert.AreEqual(false, myPalindrome.IsPalindrome(ref myPalindromeArray));
+    }
   }
 }
--- Palindromes/Models/Palindromes.cs
+++ Palindromes/Models/Palindromes.cs
@@ -1,5 +1,11 @@
 
-
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.Metadata;
+using System.IO;
+using System.Security.AccessControl;
+using System.Collections.Generic;
+using System.Buffers;
+using System.Collections;
 using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
@@ -11,40 +17,78 @@ using System.Runtime.InteropServices;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.Linq;
 
 namespace Palindromes
 {
   public class Palindromes
   {
 
-    public string MyString{get; set;}
+    public string MyString{ get; set; }
 
     public static void Main()
     {
       Console.WriteLine("Please input a word:");
       string input = Console.ReadLine();
-      Palindromes myPalindromes = new Palindromes(input);
-
+      Palindromes myPalindromes = new Palindromes(input.ToLower());
+      char[] myPalindromeArray = myPalindromes.StringToArray();
+      List<char> flipped = Palindromes.FlipTheArray(ref myPalindromeArray);
+      for(int i=0;i<flipped.Count;i++)
+      {
+        Console.WriteLine(flipped[i]);
+      }
+      bool result = myPalindromes.IsPalindrome(ref myPalindromeArray);
+      Console.WriteLine(result);
     }
 
     public Palindromes(string input)
     {
-      MyString = input;
+      MyString = input.ToLower();
     }
 
   //get rid of
-    public Array StringToArray()
+    public char[] StringToArray()
     {
       string toConvert = MyString;
       char[] array = toConvert.ToCharArray();
       return array;
     }
 
-    // public Reverse(Array toReverse)
-    // {
+    public static List<char> FlipTheArray(ref char[] array)
+    {
+      // char[] arrayFlip = new char[array.Length];
+      // arrayFlip = array;
+      List<char> arrayList = array.ToList();
+      List<char> flippedList = new List<char>();
+      for (int i = array.Length -1; i >= 0; --i)
+      {
+        // toAdd = arrayp[i];
+        // flippedList.Add(toAdd);
+        flippedList.Add(arrayList[i]);
+      }
+      return flippedList;
+    }
+
+    public bool IsPalindrome(ref char[] array)
+    {
       
-    // }
+      List<char> arrayList = array.ToList();
+      List<char> flipped = Palindromes.FlipTheArray(ref array);
+
+      for(int i=0;i<arrayList.Count;i++)
+      {
+        Console.WriteLine(arrayList[i]);
+      }
+      for(int i=0;i<flipped.Count;i++)
+      {
+        Console.WriteLine(flipped[i]);
+      }
+
+    return arrayList.SequenceEqual(flipped);
+    }
   }
-
-
 }
+
+
+
+
